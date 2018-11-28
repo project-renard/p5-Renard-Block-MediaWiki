86519521e8f3e3b06aca058130aaaf6f322e86cb
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-11-28 16:02:41 -0600

    Add maint files

---
+++ lib/Renard/Incunabula/Block/MediaWiki.pm
@@ -0,0 +1,14 @@
+package Renard::Incunabula::Block::MediaWiki;
+# ABSTRACT: A source for MediaWiki data
+
+use strict;
+use warnings;
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Incunabula-Block-MediaWiki/>
+
+=cut
--- lib/Renard/Incunabula/Source/MediaWiki.pm
+++
@@ -1,6 +0,0 @@
-package Renard::Incunabula::Source::MediaWiki;
-
-use strict;
-use warnings;
-
-1;
