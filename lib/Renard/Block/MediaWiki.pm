3ac55d73568151252b5b52c1277717f52d691929
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-12-07 23:00:56 -0600

    Remove ::Incunabula:: from namespace

---
+++ lib/Renard/Block/MediaWiki.pm
@@ -0,0 +1,14 @@
+package Renard::Block::MediaWiki;
+# ABSTRACT: A source for MediaWiki data
+
+use strict;
+use warnings;
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Block-MediaWiki/>
+
+=cut
